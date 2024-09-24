de83383f9139b8b8dba89124ff661192f13fa480
Joyyh20 <joyyh20@gmail.com>
2024-09-24 11:45:55 -0300

    Bomba pra corrigir

--- And, or not Teste.py
+++ And, or not Teste.py
@@ -10,10 +10,9 @@ if posição == 1:
     print("Campeão")
 elif posição > 1 and posição <= 6:
     print("Libertadores!")
-elif posição > 7 and posição <= 12:
+elif posição >= 7 and posição <= 12:
     print("Sul-Americana")
 elif posição >= 17:
     print("Rebaixado")
 else:
     print("Não sei dizer")
-    
