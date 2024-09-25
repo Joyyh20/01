bf9f3b519e9ad2bb7faa8cc05c8d0f3185e0d454
Joyyh20 <joyyh20@gmail.com>
2024-09-25 08:37:41 -0300

    fogo

---
+++ Hora certa.py
@@ -0,0 +1,15 @@
+#Solção eclipse solar
+
+hora = input("Qual a hora de chegada?")
+estado = input("Qual o estado?")
+dia = input("O dia está claro(S/N)?")
+
+if hora == "15:25" and estado == "RJ" and dia == "5":
+    print("Eclipse total")
+elif hora > "15:20" and hora > "13:30":
+    if estado == "RJ" or estado == "SP" or estado == "MG" or estado:
+    
+if dia =="5":
+       print("Eclipse Parcial")
+else:
+    print("Não deu pra ver o eclipse")
